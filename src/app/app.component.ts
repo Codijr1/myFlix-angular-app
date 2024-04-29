e5e42733f68851e4e9ff3d67147e935880fd019a
Codi <cjrobards@gmail.com>
2024-04-29 13:06:58 -0300

    Welcome Page css fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'myFlix-Angular-client';
-  navbarVisible = true; // Property to track if the navbar should be visible
+  currentRoute: string = '';
 
   constructor(
-    public dialog: MatDialog,
-    private router: Router // Inject the router to track navigation changes
+    private router: Router,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
-    // Hide the navbar on specific routes
-    const navbarHiddenRoutes = ['/welcome']; // Define routes to hide the navbar
-
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.navbarVisible = !navbarHiddenRoutes.includes(this.router.url);
+        this.currentRoute = event.urlAfterRedirects;
       }
     });
   }
 
+  isWelcomePage(): boolean {
+    return this.currentRoute === '/welcome' || this.currentRoute === '/';
+  }
+
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
+      width: '280px',
     });
   }
 
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
+      width: '280px',
     });
   }
 }
